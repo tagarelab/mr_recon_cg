492d824aec4274aafac9c3ee63ab02d71da2ce90
hsun981 <heng.sun@vanderbilt.edu>
2024-02-15 13:00:42 -0500

    Added a validation method for CG result

---
+++ validation.py
@@ -0,0 +1,18 @@
+"""
+   Name: validation.py
+   Purpose: Validation functions for the project
+   Created on: 2/13/2024
+   Created by: Heng Sun
+   Additional Notes: 
+"""
+import numpy as np
+
+
+def validate_equal(a,b,Name_a = "First Item", Name_b = "Second Item", atol = 1e-8, rtol = 1e-5, equal_nan = False):
+    """
+    Validate if two items are equal
+    """
+    if np.allclose(a, b, atol = atol, rtol = rtol, equal_nan = equal_nan):
+        print(f"{Name_a} and {Name_b} are equal")
+    else:
+        print(f"{Name_a} and {Name_b} are not equal")
