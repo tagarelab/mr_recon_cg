099c26d97dcbd93b1c56fb83643b84c0a6bd9093
hsun981 <heng.sun@vanderbilt.edu>
2024-02-21 11:31:57 -0500

    fixed typo - test needed

--- algebra.py
+++ algebra.py
@@ -7,6 +7,7 @@
 """
 
 import numpy as np
+import scipy as sp
 
 
 def interpolate_3D(signal, target_shape=None, factor=1):
@@ -28,28 +29,28 @@ def interpolate_3D(signal, target_shape=None, factor=1):
     if len(signal.shape) != 3 or len(target_shape) != 3:
         raise ValueError("The input and target shapes must both be 3D.")
 
-        # Define the original grid
-        x_orig = np.arange(signal.shape[0])
-        y_orig = np.arange(signal.shape[1])
-        z_orig = np.arange(signal.shape[2])
+    # Define the original grid
+    x_orig = np.arange(signal.shape[0])
+    y_orig = np.arange(signal.shape[1])
+    z_orig = np.arange(signal.shape[2])
 
-        # Define the new grid
-        x_new = np.linspace(0, signal.shape[0], target_shape[0])
-        y_new = np.linspace(0, signal.shape[1], target_shape[1])
-        z_new = np.linspace(0, signal.shape[2], target_shape[2])
+    # Define the new grid
+    x_new = np.linspace(0, signal.shape[0], target_shape[0])
+    y_new = np.linspace(0, signal.shape[1], target_shape[1])
+    z_new = np.linspace(0, signal.shape[2], target_shape[2])
 
-        # Create the interpolator function
-        interp_func = RegularGridInterpolator((x_orig, y_orig, z_orig), signal)
+    # Create the interpolator function
+    interp_func = sp.interpolate.RegularGridInterpolator((x_orig, y_orig, z_orig), signal)
 
-        # Define the points where you want to interpolate
-        points = np.mgrid[x_new[0]:x_new[-1]:target_shape[0] * 1j, y_new[0]:y_new[-1]:target_shape[1] * 1j,
-                 z_new[0]:z_new[-1]:target_shape[2] * 1j]
-        points = points.reshape(3, -1).T
+    # Define the points where you want to interpolate
+    points = np.mgrid[x_new[0]:x_new[-1]:target_shape[0] * 1j, y_new[0]:y_new[-1]:target_shape[1] * 1j,
+             z_new[0]:z_new[-1]:target_shape[2] * 1j]
+    points = points.reshape(3, -1).T
 
-        # Get the interpolated values
-        interpolated_values = interp_func(points)
+    # Get the interpolated values
+    interpolated_values = interp_func(points)
 
-        return interpolated_values.reshape(target_shape)
+    return interpolated_values.reshape(target_shape)
 
 
 
