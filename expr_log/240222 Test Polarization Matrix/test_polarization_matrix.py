ccd8279ab0fbac201eadb19cd30702f4fe603328
hsun981 <heng.sun@vanderbilt.edu>
2024-02-22 13:49:55 -0500

    method to create 3d scatter plot and some simple test experiemnt for slice selection functions

---
+++ expr_log/240222 Test Polarization Matrix/test_polarization_matrix.py
@@ -0,0 +1,36 @@
+"""
+   Name: test_polarization_matrix.py
+   Purpose:
+   Created on: 2/22/2024
+   Created by: Heng Sun
+   Additional Notes: 
+"""
+
+import gen_op
+from sim import acquisition as acq
+import mr_io
+import numpy as np
+import matplotlib.pyplot as plt
+import visualization as vis
+
+path = 'sim_inputs/magnetData.csv'
+nubo_b0_raw, b0_X, b0_Y, b0_Z = mr_io.read_nubo_b0(path=path, intrp_x=30, intrp_y=30, intrp_z=30)
+
+vis.scatter3d(b0_X, b0_Y, b0_Z, nubo_b0_raw)
+
+# %% slice selection
+# Constants
+gamma = 42.58e6  # Hz/T
+read_mag = 0.046  # T
+DC = 0.1  # percent DC when polarizing
+ctr_mag = read_mag  # slice selection gradient
+slc_tkns_frq = 100e3 * 2  # Hz
+slc_tkns_mag = slc_tkns_frq / gamma  # T
+
+# Adjust nubo_b0
+nubo_b0 = nubo_b0_raw * DC  # slice strength
+
+# Call slice_select function
+id = acq.slice_select(nubo_b0, ctr_mag, slc_tkns_mag)
+
+vis.scatter3d(b0_X, b0_Y, b0_Z, nubo_b0_raw, mask=id)
---
+++ visualization.py
@@ -0,0 +1,48 @@
+"""
+   Name: visualization.py
+   Purpose:
+   Created on: 2/22/2024
+   Created by: Heng Sun
+   Additional Notes: 
+"""
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+
+def scatter3d(B0_SI, B0_LR, B0_AP, grad, mask=None):
+    """
+    3D scatter plot
+    This function is adapted from a MATLAB function by Github Copilot and edited & tested by the author.
+
+    Parameters:
+    - B0_SI (numpy.ndarray): The B0 field map in the SI direction.
+    - B0_LR (numpy.ndarray): The B0 field map in the LR direction.
+    - B0_AP (numpy.ndarray): The B0 field map in the AP direction.
+    - grad (numpy.ndarray): The gradient.
+    - grad_str (str): The gradient string.
+
+    Returns:
+    - None
+    """
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    X_M, Y_M, Z_M = np.meshgrid(B0_SI, B0_LR, B0_AP, indexing='ij')
+
+    if mask is not None:
+        X_M = X_M[mask]
+        Y_M = Y_M[mask]
+        Z_M = Z_M[mask]
+        grad = grad[mask]
+
+    scatter = ax.scatter(X_M, Y_M, Z_M, c=grad * 1e3, s=1)
+    plt.colorbar(scatter)
+
+    # ax.set_title("Liver Gradient at "+grad_str+" mT/m")
+    ax.set_xlabel('SI (m)')
+    ax.set_ylabel('LR (m)')
+    ax.set_zlabel('AP (m)')
+    ax.axis('equal')
+
+    plt.show()
