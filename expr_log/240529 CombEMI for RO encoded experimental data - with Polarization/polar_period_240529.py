9eab529e9640b78c2fca2dc8f13fce879de7c78b
hsun981 <heng.sun@vanderbilt.edu>
2024-06-04 09:33:03 -0400

    new experiment about polarization

--- expr_log/240529 CombEMI for RO encoded experimental data - with Polarization/polar_period_240529.py
+++ expr_log/240529 CombEMI for RO encoded experimental data - with Polarization/polar_period_240529.py
@@ -7,12 +7,33 @@
 """
 
 import numpy as np
+import scipy as sp
+import visualization as vis
 
 # plot data
 ylim_time = [-15000, 15000]
-ylim_freq = [-3e5, 3e5]
+ylim_freq = [-5e6, 5e6]
 ylim_freq_zfilled = [-1e8, 1e8]
 Disp_Intermediate = True
+
+# %% Data parameters
+# N_echoes = 479
+TE = 2e-3
+dt = 6e-6
+pre_drop = 0
+post_drop = 0
+pk_win = 0.33
+pk_id = None  # None for auto peak detection
+polar_time = 0
+
+max_iter = 200
+rho = 1
+lambda_val = -1  # -1 for auto regularization
+# auto lambda parameters
+ft_prtct = 20
+
+
+# echo_len = int(raw_sig_all.shape[0] / N_echoes)
 
 
 # %% support functions
@@ -25,7 +46,10 @@ def avg_first_k_peaks(signal, echo_len, k=10):
 
 
 # %% Load raw data
-file_name = 'WithRO_16Scans'
+file_name = 'NoRO_16Scans'
 mat_file = sp.io.loadmat('sim_input/' + file_name + '.mat')
-raw_sig_all = mat_file['ch1'][1100:, :]  # the first 1000 points are polarization period
-comb_sig_all = np.zeros(raw_sig_all.shape, dtype='complex')
+raw_pol = mat_file['ch1'][:1000, :]  # the first 1000 points are polarization period
+
+vis.freq_plot(raw_pol[:, 0], dt=dt, name="Polarization Period, No Gradient", ylim=ylim_freq)
+vis.freq_plot(raw_pol[:, 1], dt=dt, name="Polarization Period, No Gradient", ylim=ylim_freq)
+vis.freq_plot(raw_pol[:, 2], dt=dt, name="Polarization Period, No Gradient", ylim=ylim_freq)
