ee7635565161f796f9116d37ebbf15f50d14c44c
hsun981 <heng.sun@vanderbilt.edu>
2024-09-09 13:18:15 -0400

    load the entire folder

---
+++ expr_log/240904 EDITER evaluation/tnmr2_mat_240904.py
@@ -0,0 +1,20 @@
+"""
+   Name: load_raw_data.py
+   Purpose:
+   Created on: 5/21/2024
+   Created by: Heng Sun
+   Additional Notes: 
+"""
+
+# import TNMR_dealer as td
+from denoise import editer
+
+# Change to the actual data location
+loc = "C:\\Yale\\MRRC\\mr_recon_cg\\data\\240828_EMI_hunt_S3E4\\EMI_Probe_Pos_2\\"
+interleave = 4  # Change to the actual interleave number
+N_ch = 4  # Change to the actual number of channels
+rep = 1  # Change to the actual number of separate .tnt files
+seg = 1  # Change to the actual number of segments in one .tnt files that will be saved in separate rows, usually 1
+sig_ch_name = 'ch2'  # Change to the actual signal channel name
+emi_ch_name = ['ch1', 'ch3', 'ch4']  # Change to the actual EMI channel names
+editer.editer_process_all_files(loc, interleave, N_ch, rep, seg, sig_ch_name, emi_ch_name, new_file_prefix='Pos2_')
