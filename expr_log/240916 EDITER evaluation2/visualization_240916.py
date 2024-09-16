e97d7b1866e28774e5e7b998cee05b1896378856
hsun981 <heng.sun@vanderbilt.edu>
2024-09-16 15:33:24 -0400

    visualization for results of S2E5

--- expr_log/240916 EDITER evaluation2/visualization_240916.py
+++ expr_log/240916 EDITER evaluation2/visualization_240916.py
@@ -15,52 +15,74 @@ import mr_io
 # %% Load data
 loc = "C:\\Yale\\MRRC\\mr_recon_cg\\data\\240911_EMI_hunt_S2E5\\EDITER\\"
 
-dict = {
-    0: "Pos1_NoPhant_B0off_prepol_0_slice_0_PAoff.mat",
-    1: "Pos1_NoPhant_B0on_prepol_0_slice_0.mat",
-    2: "Pos1_NoPhant_B0on_prepol_0_slice_0_PAoff.mat",
-    3: "Pos1_NoPhant_B0on_prepol_0_slice_3.mat",
-    4: "Pos1_NoPhant_B0on_prepol_0_slice_6.3.mat",
-    5: "Pos1_NoPhant_B0on_prepol_100_slice_0.mat",
-    6: "Pos1_NoPhant_B0on_prepol_100_slice_3.mat",
-    7: "Pos1_NoPhant_B0on_prepol_100_slice_6.3.mat",
-    8: "Pos1_NoPhant_B0on_prepol_100_slice_6.3_32avg.mat",
-    9: "Pos1_NoPhant_B0pwroff_prepol_0_slice_0_PAoff.mat",
-    10: "Pos1_WithPhant_B0on_prepol_0_slice_0.mat",
-    11: "Pos1_WithPhant_B0on_prepol_0_slice_3.mat",
-    12: "Pos1_WithPhant_B0on_prepol_0_slice_6.3.mat",
-    13: "Pos1_WithPhant_B0on_prepol_100_slice_0.mat",
-    14: "Pos1_WithPhant_B0on_prepol_100_slice_3.mat",
-    15: "Pos1_WithPhant_B0on_prepol_100_slice_6.3.mat",
-    16: "Pos1_WithPhant_B0on_prepol_100_slice_6.3_32avg.mat",
-    17: "Pos2_WithPhant_B0on_prepol_0_slice_0.mat",
-    18: "Pos2_WithPhant_B0on_prepol_0_slice_3.mat",
-    19: "Pos2_WithPhant_B0on_prepol_0_slice_6.3.mat",
-    20: "Pos2_WithPhant_B0on_prepol_100_slice_0.mat",
-    21: "Pos2_WithPhant_B0on_prepol_100_slice_3.mat",
-    22: "Pos2_WithPhant_B0on_prepol_100_slice_6.3.mat",
-    23: "Pos2_WithPhant_B0on_prepol_100_slice_6.3_32avg.mat"
+file_dict = {
+    0: "Pos2_NoPhant_B0off_prepol_0_slice_0_PAoff.mat",
+    1: "Pos2_NoPhant_B0on_prepol_0_slice_0_1.mat",
+    2: "Pos2_NoPhant_B0on_prepol_0_slice_0_2.mat",
+    3: "Pos2_NoPhant_B0on_prepol_0_slice_0_3.mat",
+    4: "Pos2_NoPhant_B0on_prepol_0_slice_0_PAoff.mat",
+    5: "Pos2_NoPhant_B0on_prepol_0_slice_3_1.mat",
+    6: "Pos2_NoPhant_B0on_prepol_0_slice_3_2.mat",
+    7: "Pos2_NoPhant_B0on_prepol_0_slice_3_3.mat",
+    8: "Pos2_NoPhant_B0on_prepol_0_slice_6.3_1.mat",
+    9: "Pos2_NoPhant_B0on_prepol_0_slice_6.3_2.mat",
+    10: "Pos2_NoPhant_B0on_prepol_0_slice_6.3_3.mat",
+    11: "Pos2_NoPhant_B0on_prepol_100_slice_0_1.mat",
+    12: "Pos2_NoPhant_B0on_prepol_100_slice_0_2.mat",
+    13: "Pos2_NoPhant_B0on_prepol_100_slice_0_3.mat",
+    14: "Pos2_NoPhant_B0on_prepol_100_slice_3_1.mat",
+    15: "Pos2_NoPhant_B0on_prepol_100_slice_3_2.mat",
+    16: "Pos2_NoPhant_B0on_prepol_100_slice_3_3.mat",
+    17: "Pos2_NoPhant_B0on_prepol_100_slice_6.3_1.mat",
+    18: "Pos2_NoPhant_B0on_prepol_100_slice_6.3_2.mat",
+    19: "Pos2_NoPhant_B0on_prepol_100_slice_6.3_3.mat",
+    20: "Pos2_NoPhant_B0pwroff_prepol_0_slice_0_PAoff.mat",
+    21: "Pos1_NoPhant_B0off_prepol_0_slice_0_PAoff.mat",
+    22: "Pos1_NoPhant_B0on_prepol_0_slice_0_1.mat",
+    23: "Pos1_NoPhant_B0on_prepol_0_slice_0_2.mat",
+    24: "Pos1_NoPhant_B0on_prepol_0_slice_0_3.mat",
+    25: "Pos1_NoPhant_B0on_prepol_0_slice_0_PAoff.mat",
+    26: "Pos1_NoPhant_B0on_prepol_0_slice_3_1.mat",
+    27: "Pos1_NoPhant_B0on_prepol_0_slice_3_2.mat",
+    28: "Pos1_NoPhant_B0on_prepol_0_slice_3_3.mat",
+    29: "Pos1_NoPhant_B0on_prepol_0_slice_6.3_1.mat",
+    30: "Pos1_NoPhant_B0on_prepol_0_slice_6.3_2.mat",
+    31: "Pos1_NoPhant_B0on_prepol_0_slice_6.3_3.mat",
+    32: "Pos1_NoPhant_B0on_prepol_100_slice_0_1.mat",
+    33: "Pos1_NoPhant_B0on_prepol_100_slice_0_2.mat",
+    34: "Pos1_NoPhant_B0on_prepol_100_slice_0_3.mat",
+    35: "Pos1_NoPhant_B0on_prepol_100_slice_3_1.mat",
+    36: "Pos1_NoPhant_B0on_prepol_100_slice_3_2.mat",
+    37: "Pos1_NoPhant_B0on_prepol_100_slice_3_3.mat",
+    38: "Pos1_NoPhant_B0on_prepol_100_slice_6.3_1.mat",
+    39: "Pos1_NoPhant_B0on_prepol_100_slice_6.3_2.mat",
+    40: "Pos1_NoPhant_B0on_prepol_100_slice_6.3_3.mat",
+    41: "Pos1_NoPhant_B0pwroff_prepol_0_slice_0_PAoff.mat",
 }
-# B0 originated noise
+# # B0 originated noise
 # names = ["B0 Disconnected", "B0 Connected to Power", "B0 Turned On",
 #          "PA also Turned On", "RO 3%", "RO 6.3%"]
-# ids = [9, 0, 2, 1, 3, 4]
+# ids = [41, 21, 25, 22, 26, 29] # position 1
+# # ids = [20, 0, 4, 1, 5, 8]# position 2
+# position = "Position 1"
 
 # Prepol noise
 names = ["B0 Disconnected", "Prepol off, RO 0%", "Prepol off, RO 3%", "Prepol off RO 6.3%", "Prepol on, RO 0%",
          "Prepol on, RO 3%", "Prepol on, RO 6.3%"]
-ids = [9, 1, 3, 4, 5, 6, 7]
+ids = [20, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]  # position 2
+# ids = [41, 22, 23, 24, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]  # position 1
+position = "Position 2"
 
 Nc = 3
 ylim_temp = [0, 1000]
 ylim_temp_freq = [-60, 60]
 ylim_temp_freq2 = None
 
-power_raw = np.zeros(len(names))
-power_corr = np.zeros(len(names))
+power_raw = np.zeros(len(ids))
+power_corr = np.zeros(len(ids))
 
-for i in range(len(names)):
-    dict_i = mr_io.load_single_mat('sim_input/' + dict[ids[i]])
+for i in range(len(ids)):
+    dict_i = mr_io.load_single_mat(name=file_dict[ids[i]], path=loc)
     raw_sig_i = dict_i['raw_sig']
     editer_corr_i = dict_i['editer_corr']
     diff = editer_corr_i - raw_sig_i
@@ -75,19 +97,35 @@ for i in range(len(names)):
     #               ifft=True,
     #               ylim=ylim_temp_freq)
 
-tick_positions = range(len(names))
-
-plt.figure()
-plt.plot(tick_positions, power_raw, label='Raw')
-plt.plot(tick_positions, power_corr, label='Corrected')
-plt.legend()
-plt.ylim([0, 5.5e6])
-plt.xticks(tick_positions, names)
-plt.gca().tick_params(axis='x', rotation=15)
-plt.ylabel('Noise Power')
-plt.title('Noise Power Before and After EDITER Correction')
-plt.show()
+# %% Plot
+# tick_positions = range(len(names))
+#
+# plt.figure()
+# plt.plot(tick_positions, power_raw, label='Raw')
+# plt.plot(tick_positions, power_corr, label='Corrected')
+# plt.legend()
+# plt.ylim([0, 5.5e6])
+# plt.xticks(tick_positions, names)
+# plt.gca().tick_params(axis='x', rotation=15)
+# plt.ylabel('Noise Power')
+# plt.title(position)
+# plt.show()
 
 corr_diff = power_corr[-1] - power_corr[0]
 raw_diff = power_raw[-1] - power_raw[0]
 print(100 * (raw_diff - corr_diff) / raw_diff)
+
+# Scatter plot
+tick_positions = [0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6]
+tick_to_name = range(len(names))
+
+plt.figure()
+plt.scatter(tick_positions, power_raw, label='Raw')
+plt.scatter(tick_positions, power_corr, label='Corrected')
+plt.legend()
+plt.ylim([0, 5.5e6])
+plt.xticks(tick_to_name, names)
+plt.gca().tick_params(axis='x', rotation=15)
+plt.ylabel('Noise Power')
+plt.title(position)
+plt.show()
