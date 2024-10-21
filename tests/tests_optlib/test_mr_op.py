9cf43feabfbdd6739631489a34a9ea4dfd0477f5
hsun981 <heng.sun@vanderbilt.edu>
2024-10-21 09:40:21 -0400

    implement test projection operator

--- tests/tests_optlib/test_mr_op.py
+++ tests/tests_optlib/test_mr_op.py
@@ -9,6 +9,9 @@ from unittest import TestCase
 import numpy as np
 import test_operators as test_ops
 from optlib import mr_op as mr_op
+import visualization as vis
+import algebra as algb
+from sim import masks as mks
 
 
 class Test_rotation_op(TestCase):
@@ -55,3 +58,48 @@ class Test_detection_op(TestCase):
 
     def test_transpose(self):
         self.fail()
+
+
+class Test_projection_op(TestCase):
+
+    def test_adjoint_property(self):
+        # generate a random projection operator
+        # generate random 3D boolean mask
+        mask = np.random.rand(10, 10, 10) > 0.5
+        op = mr_op.projection_op(mask_3D=mask)
+        test_ops.test_adjoint_property(op)
+
+    def test_forward(self):
+        # Create sample data (3D array
+        data_shape = (5, 5, 5)
+        data = np.random.random(data_shape)  # Example shape (4, 3, 5)
+        x_axis = np.arange(data.shape[0])
+        y_axis = np.arange(data.shape[1])
+        z_axis = np.arange(data.shape[2])
+
+        # Create a sample mask (3D boolean array)
+        mask_3D = (data > 0.8)  # Random boolean mask
+
+        # Extract the elements of data that match the mask
+        x = data[mask_3D]
+
+        # Create the projection operator
+        op = mr_op.projection_op(mask_3D)
+
+        # Apply the operator to the data
+        y = op.forward(x)
+
+        # Check that the output has the correct shape
+        self.assertEqual(y.shape, op.get_y_shape())
+
+        # visually check the output
+        # vis.scatter3d(x, x_axis=x_axis, y_axis=y_axis, z_axis=z_axis, mask=mask_3D)
+        # vis.scatter2d(y, x_axis=x_axis, y_axis=y_axis, mask=np.nonzero(op.mask_tkns))
+
+        # Check that the output has the correct values
+        x_matrix = mks.mask2matrix(x, mask_3D, matrix_shape=data_shape)
+        y_expected = np.sum(x_matrix, axis=2)
+        pass
+
+    def test_transpose(self):
+        pass
